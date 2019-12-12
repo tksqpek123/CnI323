29593b765a31b3e1e7522b2e50aa366d0d1e80b0
tksqpek123 <greg00@hanmail.net>
2019-12-13 03:19:10 +0900

    Initial Commit

---
+++ CnI323/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CnI323
+//
+//  Created by クォンスンギュン on 2019/12/13.
+//  Copyright © 2019 cni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
